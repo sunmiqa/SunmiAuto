0a7239b955fc2ced2169407eabc170edb98cd1dc
zhangjiyang <zhangjiyang@sunmi.com>
2017-07-07 14:16:00 +0800

    add SDK value to judge android verison ,if >=6.0 ,i-2,if < 6.0 ,i-3; fix the question"if screenshot dir is null" fix the premission question "change the targetsdk to 22"

--- app/src/androidTest/java/sunmi/com/sunmiauto/UiAuto.java
+++ app/src/androidTest/java/sunmi/com/sunmiauto/UiAuto.java
@@ -33,7 +33,7 @@ import javax.mail.MessagingException;
  */
 
 public class UiAuto {
-    static Instrumentation instrumentation =InstrumentationRegistry.getInstrumentation();
+    static Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
     static UiDevice device = UiDevice.getInstance(instrumentation);
 
 
@@ -41,6 +41,7 @@ public class UiAuto {
     //每条测试用例开始前执行操作
     public void setup() throws RemoteException, UiObjectNotFoundException, IOException {
         device.executeShellCommand("am start -n woyou.market/store.ui.activity.HomeActivity");
+        sleep(2000);
         device.findObject(By.text("推荐")).click();
     }
 
@@ -53,27 +54,27 @@ public class UiAuto {
 
     @BeforeClass
     //该测试类开始前执行操作
-    public static void initLiza() throws RemoteException {
-        File dir=new File(Environment.getExternalStorageDirectory()+File.separator+"app_spoon-screenshots");
+    public static void initLiza() throws RemoteException, IOException {
+        File dir = new File(Environment.getExternalStorageDirectory() + File.separator + "app_spoon-screenshots");
+        Log.v("testDir", Environment.getExternalStorageDirectory() + File.separator + "app_spoon-screenshots");
         class Inner {
             //递归删除文件夹
-            private void deleteFile(File file) {
-                if (file.exists()) {//判断文件是否存在
-                    if (file.isFile()) {//判断是否是文件
-                        file.delete();//删除文件
-                    } else if (file.isDirectory()) {//否则如果它是一个目录
-                        File[] files = file.listFiles();//声明目录下所有的文件 files[];
-                        for (int i = 0;i < files.length;i ++) {//遍历目录下所有的文件
-                            this.deleteFile(files[i]);//把每个文件用这个方法进行迭代
-                        }
-                        file.delete();//删除文件夹
-                    }
+            private void deleteAll(File file) {
+                if(!file.exists()){
+                    return;
+                }
+                if (file.isFile() || file.list().length == 0) {
+                    file.delete();
                 } else {
-                    System.out.println("所删除的文件不存在");
+                    File[] files = file.listFiles();
+                    for (File f : files) {
+                        deleteAll(f);//递归删除每一个文件
+                        f.delete();//删除该文件夹
+                    }
                 }
             }
         }
-        new Inner().deleteFile(dir);
+        new Inner().deleteAll(dir);
         dir.mkdir();
     }
 
@@ -202,35 +203,45 @@ public class UiAuto {
 //        sleep(2000);
 //    }
 
-    public void screenshotCap(String tag){
+    public void screenshotCap(String tag) {
         StackTraceElement testClass = findTestClassTraceElement(Thread.currentThread().getStackTrace());
         String className = testClass.getClassName().replaceAll("[^A-Za-z0-9._-]", "_");
         String methodName = testClass.getMethodName();
-        Log.v("Extenrnal",Environment.getExternalStorageState());
-        File dir=new File(Environment.getExternalStorageDirectory()
-                +File.separator+"app_spoon-screenshots"+File.separator
-                +className+File.separator+methodName);
+        Log.v("Extenrnal", Environment.getExternalStorageState());
+        File dir = new File(Environment.getExternalStorageDirectory()
+                + File.separator + "app_spoon-screenshots" + File.separator
+                + className + File.separator + methodName);
         Log.v("TEST", dir.getAbsolutePath());
-        if(!dir.exists()){
+        if (!dir.exists()) {
             dir.mkdirs();
         }
         String screenshotName = System.currentTimeMillis() + "_" + tag + ".png";
         File screenshotFile = new File(dir, screenshotName);
-        device.takeScreenshot(screenshotFile,0.2F,50);
+        device.takeScreenshot(screenshotFile, 0.2F, 50);
         sleep(2000);
     }
 
 
-
     StackTraceElement findTestClassTraceElement(StackTraceElement[] trace) {
-        for(int i = trace.length - 1; i >= 0; --i) {
+        for (StackTraceElement e : trace) {
+            Log.v("traceTest", e.getClassName() + ":" + e.getMethodName());
+        }
+        for (int i = trace.length - 1; i >= 0; --i) {
             StackTraceElement element = trace[i];
-            if("android.test.InstrumentationTestCase".equals(element.getClassName()) && "runMethod".equals(element.getMethodName())) {
-                return trace[i - 3];
+            if ("android.test.InstrumentationTestCase".equals(element.getClassName()) && "runMethod".equals(element.getMethodName())) {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    return trace[i - 2];
+                } else {
+                    return trace[i - 3];
+                }
             }
 
-            if("org.junit.runners.model.FrameworkMethod$1".equals(element.getClassName()) && "runReflectiveCall".equals(element.getMethodName())) {
-                return trace[i - 3];
+            if ("org.junit.runners.model.FrameworkMethod$1".equals(element.getClassName()) && "runReflectiveCall".equals(element.getMethodName())) {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    return trace[i - 2];
+                } else {
+                    return trace[i - 3];
+                }
             }
         }
 
@@ -247,25 +258,25 @@ public class UiAuto {
 
     //测试能够成功打开应用市场
     @Test
-    public void testOpenAppStore(){
+    public void testOpenAppStore() {
 //        screenshotCap("beforeEnter");
 //        device.findObject(By.text("应用市场")).clickAndWait(Until.newWindow(),5000);
 //        device.waitForIdle(10000);
         screenshotCap("afterEnter");
         UiObject2 suggObj = device.findObject(By.res("woyou.market:id/fab_me"));
-        Assert.assertNotNull("未找到用户入口，判断打开应用市场失败",suggObj);
+        Assert.assertNotNull("未找到用户入口，判断打开应用市场失败", suggObj);
     }
 
     //测试新品应用存在
     @Test
-    public void testCheckNewArrive(){
+    public void testCheckNewArrive() {
 //        screenshotCap("beforeEnter");
 //        device.findObject(By.text("应用市场")).clickAndWait(Until.newWindow(),10000);
         screenshotCap("afterEnter");
-        device.findObject(By.res("woyou.market:id/tv_newest_all").text("全部")).clickAndWait(Until.newWindow(),10000);
+        device.findObject(By.res("woyou.market:id/tv_newest_all").text("全部")).clickAndWait(Until.newWindow(), 10000);
         screenshotCap("afterClickNewestAll");
         UiObject2 newestAllObj = device.findObject(By.res("woyou.market:id/list_view"));
-        Assert.assertNotNull("不存在",newestAllObj);
+        Assert.assertNotNull("不存在", newestAllObj);
     }
 
     //测试新品应用滑动查看
@@ -276,20 +287,20 @@ public class UiAuto {
         screenshotCap("afterEnter");
         UiScrollable newAllScroll = new UiScrollable(new UiSelector().resourceId("woyou.market:id/recycler_view_newest"));
         newAllScroll.setAsHorizontalList();
-        newAllScroll.scrollToEnd(20,10);
-        newAllScroll.scrollToBeginning(20,10);
+        newAllScroll.scrollToEnd(20, 10);
+        newAllScroll.scrollToBeginning(20, 10);
     }
 
     //测试热门中全部应用查看
     @Test
-    public void testCheckHotApps(){
+    public void testCheckHotApps() {
 //        screenshotCap("beforeEnter");
 //        device.findObject(By.text("应用市场")).clickAndWait(Until.newWindow(),10000);
         screenshotCap("afterEnter");
-        device.findObject(By.res("woyou.market:id/tv_hot_all").text("全部")).clickAndWait(Until.newWindow(),10000);
+        device.findObject(By.res("woyou.market:id/tv_hot_all").text("全部")).clickAndWait(Until.newWindow(), 10000);
         screenshotCap("afterClickHotAll");
         UiObject2 hotAllObj = device.findObject(By.res("woyou.market:id/list_view"));
-        Assert.assertNotNull("找不到res为woyou.market:id/list_view的对象",hotAllObj);
+        Assert.assertNotNull("找不到res为woyou.market:id/list_view的对象", hotAllObj);
     }
 
     //测试热门应用滑动查看
@@ -299,8 +310,8 @@ public class UiAuto {
 //        device.findObject(By.text("应用市场")).clickAndWait(Until.newWindow(),10000);
         screenshotCap("afterEnter");
         UiScrollable hotAllScroll = new UiScrollable(new UiSelector().resourceId("woyou.market:id/linear_hot_view"));
-        hotAllScroll.scrollToEnd(20,10);
-        hotAllScroll.scrollToBeginning(20,10);
+        hotAllScroll.scrollToEnd(20, 10);
+        hotAllScroll.scrollToBeginning(20, 10);
     }
 
     //测试分类信息正确
@@ -309,27 +320,59 @@ public class UiAuto {
 //        screenshotCap("beforeEnter");
 //        device.findObject(By.text("应用市场")).clickAndWait(Until.newWindow(),10000);
         screenshotCap("afterEnter");
-        device.findObject(By.text("分类")).clickAndWait(Until.newWindow(),10000);
+        device.findObject(By.text("分类")).clickAndWait(Until.newWindow(), 10000);
         screenshotCap("enterCategory");
         UiScrollable cateScroll = new UiScrollable(new UiSelector().resourceId("woyou.market:id/recycler_view"));
         ArrayList<String> appCategory = new ArrayList<>();
-        switch (Build.DEVICE){
+        switch (Build.DEVICE) {
             case "V1-B18":
-                Log.v("enterV1-B18","V1-B18");
-                appCategory.add("外卖");appCategory.add("支付");appCategory.add("餐饮");appCategory.add("电商");appCategory.add("酒店");appCategory.add("零售");appCategory.add("工具");appCategory.add("配送");appCategory.add("旅游");appCategory.add("理财");appCategory.add("EET");
+                Log.v("enterV1-B18", "V1-B18");
+                appCategory.add("外卖");
+                appCategory.add("支付");
+                appCategory.add("餐饮");
+                appCategory.add("电商");
+                appCategory.add("酒店");
+                appCategory.add("零售");
+                appCategory.add("工具");
+                appCategory.add("配送");
+                appCategory.add("旅游");
+                appCategory.add("理财");
+                appCategory.add("EET");
                 break;
             case "V1s-G":
-                Log.v("enterV1s-G","V1s-G");
-                appCategory.add("外卖");appCategory.add("支付");appCategory.add("团购");appCategory.add("餐饮");appCategory.add("电商");appCategory.add("酒店");appCategory.add("零售");appCategory.add("工具");appCategory.add("配送");appCategory.add("旅游");appCategory.add("理财");appCategory.add("EET");
+                Log.v("enterV1s-G", "V1s-G");
+                appCategory.add("外卖");
+                appCategory.add("支付");
+                appCategory.add("团购");
+                appCategory.add("餐饮");
+                appCategory.add("电商");
+                appCategory.add("酒店");
+                appCategory.add("零售");
+                appCategory.add("工具");
+                appCategory.add("配送");
+                appCategory.add("旅游");
+                appCategory.add("理财");
+                appCategory.add("EET");
                 break;
             default:
-                Log.v("enterDefault","default");
-                appCategory.add("外卖");appCategory.add("支付");appCategory.add("团购");appCategory.add("餐饮");appCategory.add("电商");appCategory.add("酒店");appCategory.add("零售");appCategory.add("工具");appCategory.add("配送");appCategory.add("旅游");appCategory.add("理财");appCategory.add("EET");
+                Log.v("enterDefault", "default");
+                appCategory.add("外卖");
+                appCategory.add("支付");
+                appCategory.add("团购");
+                appCategory.add("餐饮");
+                appCategory.add("电商");
+                appCategory.add("酒店");
+                appCategory.add("零售");
+                appCategory.add("工具");
+                appCategory.add("配送");
+                appCategory.add("旅游");
+                appCategory.add("理财");
+                appCategory.add("EET");
         }
-        for (String s:appCategory
+        for (String s : appCategory
                 ) {
             boolean b = cateScroll.scrollTextIntoView(s);
-            Assert.assertTrue(s+"--分类不存在",b);
+            Assert.assertTrue(s + "--分类不存在", b);
         }
     }
 
@@ -339,58 +382,90 @@ public class UiAuto {
 //        screenshotCap("beforeEnter");
 //        device.findObject(By.text("应用市场")).clickAndWait(Until.newWindow(),10000);
         screenshotCap("afterEnter");
-        device.findObject(By.text("分类")).clickAndWait(Until.newWindow(),10000);
+        device.findObject(By.text("分类")).clickAndWait(Until.newWindow(), 10000);
         screenshotCap("enterCategory");
         UiScrollable cateScroll = new UiScrollable(new UiSelector().resourceId("woyou.market:id/recycler_view"));
         ArrayList<String> appCategory = new ArrayList<>();
-        switch (Build.DEVICE){
+        switch (Build.DEVICE) {
             case "V1-B18":
-                Log.v("enterV1-B18","V1-B18");
-                appCategory.add("外卖");appCategory.add("支付");appCategory.add("餐饮");appCategory.add("电商");appCategory.add("酒店");appCategory.add("零售");appCategory.add("工具");appCategory.add("配送");appCategory.add("旅游");appCategory.add("理财");appCategory.add("EET");
+                Log.v("enterV1-B18", "V1-B18");
+                appCategory.add("外卖");
+                appCategory.add("支付");
+                appCategory.add("餐饮");
+                appCategory.add("电商");
+                appCategory.add("酒店");
+                appCategory.add("零售");
+                appCategory.add("工具");
+                appCategory.add("配送");
+                appCategory.add("旅游");
+                appCategory.add("理财");
+                appCategory.add("EET");
                 break;
             case "V1s-G":
-                Log.v("enterV1s-G","V1s-G");
-                appCategory.add("外卖");appCategory.add("支付");appCategory.add("团购");appCategory.add("餐饮");appCategory.add("电商");appCategory.add("酒店");appCategory.add("零售");appCategory.add("工具");appCategory.add("配送");appCategory.add("旅游");appCategory.add("理财");appCategory.add("EET");
+                Log.v("enterV1s-G", "V1s-G");
+                appCategory.add("外卖");
+                appCategory.add("支付");
+                appCategory.add("团购");
+                appCategory.add("餐饮");
+                appCategory.add("电商");
+                appCategory.add("酒店");
+                appCategory.add("零售");
+                appCategory.add("工具");
+                appCategory.add("配送");
+                appCategory.add("旅游");
+                appCategory.add("理财");
+                appCategory.add("EET");
                 break;
             default:
-                Log.v("enterDefault","default");
-                appCategory.add("外卖");appCategory.add("支付");appCategory.add("团购");appCategory.add("餐饮");appCategory.add("电商");appCategory.add("酒店");appCategory.add("零售");appCategory.add("工具");appCategory.add("配送");appCategory.add("旅游");appCategory.add("理财");appCategory.add("EET");
+                Log.v("enterDefault", "default");
+                appCategory.add("外卖");
+                appCategory.add("支付");
+                appCategory.add("团购");
+                appCategory.add("餐饮");
+                appCategory.add("电商");
+                appCategory.add("酒店");
+                appCategory.add("零售");
+                appCategory.add("工具");
+                appCategory.add("配送");
+                appCategory.add("旅游");
+                appCategory.add("理财");
+                appCategory.add("EET");
         }
-        for (String s:appCategory
+        for (String s : appCategory
                 ) {
             boolean b = cateScroll.scrollTextIntoView(s);
-            Assert.assertTrue(s+"--分类不存在",b);
+            Assert.assertTrue(s + "--分类不存在", b);
             UiObject2 sCategoryObj = device.findObject(By.text(s));
-            sCategoryObj.clickAndWait(Until.newWindow(),10000);
+            sCategoryObj.clickAndWait(Until.newWindow(), 10000);
             UiObject2 cateNmaeObj = device.findObject(By.res("woyou.market:id/tv_title"));
             String cateName = cateNmaeObj.getText();
-            Assert.assertEquals("应该是"+s+",而实际是"+cateName,s,cateName);
+            Assert.assertEquals("应该是" + s + ",而实际是" + cateName, s, cateName);
             device.pressBack();
         }
     }
 
     //测试进入我的
     @Test
-    public void testEnterMine(){
+    public void testEnterMine() {
 //        screenshotCap("beforeEnter");
 //        device.findObject(By.text("应用市场")).clickAndWait(Until.newWindow(),10000);
         screenshotCap("afterEnter");
-        device.findObject(By.res("woyou.market:id/fab_me")).clickAndWait(Until.newWindow(),10000);
+        device.findObject(By.res("woyou.market:id/fab_me")).clickAndWait(Until.newWindow(), 10000);
         UiObject2 mineObj = device.findObject(By.res("woyou.market:id/tv_title").text("我的"));
-        Assert.assertNotNull("不存在\"我的\"",mineObj);
+        Assert.assertNotNull("不存在\"我的\"", mineObj);
     }
 
     //测试从应用商店跳转到登录商米账户界面
     @Test
-    public void testEnterLoginPageFromAppStore(){
+    public void testEnterLoginPageFromAppStore() {
 //        screenshotCap("beforeEnter");
 //        device.findObject(By.text("应用市场")).clickAndWait(Until.newWindow(),10000);
         screenshotCap("afterEnter");
-        device.findObject(By.res("woyou.market:id/fab_me")).clickAndWait(Until.newWindow(),10000);
-        device.findObject(By.res("woyou.market:id/item_user_info")).clickAndWait(Until.newWindow(),10000);
+        device.findObject(By.res("woyou.market:id/fab_me")).clickAndWait(Until.newWindow(), 10000);
+        device.findObject(By.res("woyou.market:id/item_user_info")).clickAndWait(Until.newWindow(), 10000);
         screenshotCap("enterUserLoginCenter");
         String actulPkg = device.getCurrentPackageName();
-        Assert.assertEquals("本应该是com.sunmi.usercenter，而实际是"+actulPkg,"com.sunmi.usercenter",actulPkg);
+        Assert.assertEquals("本应该是com.sunmi.usercenter，而实际是" + actulPkg, "com.sunmi.usercenter", actulPkg);
     }
 
     //测试安装热门中一个应用
@@ -399,7 +474,7 @@ public class UiAuto {
 //        screenshotCap("beforeEnter");
 //        device.findObject(By.text("应用市场")).clickAndWait(Until.newWindow(),10000);
         screenshotCap("afterEnter");
-        device.findObject(By.res("woyou.market:id/tv_hot_all").text("全部")).clickAndWait(Until.newWindow(),10000);
+        device.findObject(By.res("woyou.market:id/tv_hot_all").text("全部")).clickAndWait(Until.newWindow(), 10000);
         screenshotCap("afterClickHotAll");
         UiScrollable hotAllScroll = new UiScrollable(new UiSelector().resourceId("woyou.market:id/list_view"));
         hotAllScroll.scrollTextIntoView("安装");
