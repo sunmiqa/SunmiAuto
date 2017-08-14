851107a403e4b439984084c5a8b0c284a125eee8
zhangjiyang <zhangjiyang@sunmi.com>
2017-08-14 20:21:48 +0800

    修改应用市场搜索应用根据热门中第一个应用名称来搜索 修改设置用例兼容P1

--- app/src/androidTest/java/com/sunmi/sunmiauto/testutils/TestConstants.java
+++ app/src/androidTest/java/com/sunmi/sunmiauto/testutils/TestConstants.java
@@ -12,7 +12,8 @@ public class TestConstants {
     public static final int DOWNLOAD_WAIT = 300000;//等待应用市场应用下载完成最长时间
     public static final String NETWORKNAMETEST = "AutoTestNetwork";//添加网络名称
     public static final String NETWORKPWDTEST = "autotest388";//添加网络密码
-    public static final String BT_NAME = "V1";//蓝牙名称
+    public static final String V1_BT_NAME = "V1";//蓝牙名称
+    public static final String P1_BT_NAME = "Sunmi P1";
     public static final String USER_CENTER_PKG = "com.sunmi.usercenter";//用户中心包名
     public static final String SEARCH_APP = "DUDU";//应用市场搜索内容
     public static final String USERCENTER_ACCOUNT = "zhangjiyang@sunmi.com";//用户中心账号
@@ -31,7 +32,7 @@ public class TestConstants {
             add("日期和时间");
             add("提示音和通知");
             add("语言和输入法");
-            add("打印设置");
+            add("打印");
             add("其他设置");
             add("应用程序管理");
             add("无障碍");
@@ -49,7 +50,7 @@ public class TestConstants {
             add("音量键自定义");
             add("提示音和通知");
             add("应用");
-            add("存储设备和USB");
+            add("存储设备和 USB");
             add("电池");
             add("内存");
             add("位置信息");
